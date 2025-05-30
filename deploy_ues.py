1b8cf3b1669a23ed9e899dc5dfb423a51a67ad7e
Jianwei Cheng <cheng.4@campus.tu-berlin.de>
2025-05-30 09:35:42 +0200

    g

--- deploy_ues.py
+++ deploy_ues.py
@@ -8,26 +8,21 @@ imsis = [
     "001011234567892"
 ]
 
+compose_yaml_template = "srsue_5g_zmq_template.yaml"
+ue_env_template = ".ue-env-template"
 
-base_ip = 172220035  # 172.22.0.35 → expressed as integer
 
-for i in range(1, 2):  # 5 UEs for example
+for i in range(1, 3):  # 5 UEs for example
     imsi = imsis[i-1]
-    ki = "8baf473f2f8fd09487cccbd7097c6862"
-    op = "11111111111111111111111111111111"
-    ip = f"172.22.0.{35 + i}"
+    ip = f"172.22.0.{40 + i}"
 
-    ue_name = f"ue_5g_zmq{i}"
-    conf_file = f"srslte/{ue_name}.conf"
-    yaml_file = f"{ue_name}.yaml"
+    ue_env = f".ue{i}-env"
+    ue_container_yaml = f"srsue{i}_5g_zmq.yaml"
 
     # Step 1: Create config file from template
-    with open("srslte/ue_5g_zmq_template.conf", "r") as template:
-        config = template.read()
+    with open(ue_env, "r") as env_template:
+        config = env_template.read()
         config = config.replace("IMSI_PLACEHOLDER", imsi)
-        config = config.replace("KI_PLACEHOLDER", ki)
-        config = config.replace("OP_PLACEHOLDER", op)
-        config = config.replace("UE_IP_PLACEHOLDER", ip)
 
     with open(conf_file, "w") as out:
         out.write(config)
@@ -36,20 +31,25 @@ for i in range(1, 2):  # 5 UEs for example
     compose = f"""
 version: '3'
 services:
-  {ue_name}:
-    image: docker_srsran
-    container_name: {ue_name}
+  srs{ue_name}:
+    image: docker_srslte
+    container_name: srs{ue_name}
     stdin_open: true
     tty: true
+    cap_add:
+      - NET_ADMIN
     privileged: true
     volumes:
-      - ./srsran:/mnt/srslte
+      - ./srslte:/mnt/srslte
       - /etc/timezone:/etc/timezone:ro
       - /etc/localtime:/etc/localtime:ro
     env_file:
       - .env
     environment:
       - COMPONENT_NAME={ue_name}
+    expose:
+      - "2000/tcp"
+      - "2001/tcp"
     networks:
       default:
         ipv4_address: {ip}
@@ -64,4 +64,4 @@ networks:
         out.write(compose)
 
     # Step 3: Launch container
-    os.system(f"docker compose -f {yaml_file} up -d")
+    os.system(f"docker compose -f {yaml_file} up -d && docker container attach srsue_5g_zmq")
