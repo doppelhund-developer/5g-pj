c17c25f1eff3ca782b48b5d85f3160812981066a
Jianwei Cheng <cheng.4@campus.tu-berlin.de>
2025-07-02 22:02:11 +0200

    gg works

--- kamailio_voip/kamailio_init.sh
+++ kamailio_voip/kamailio_init.sh
@@ -4,5 +4,9 @@ echo "Copying config to Kamailio directory..."
 cp /mnt/kamailio_voip/kamailio.cfg /usr/local/etc/kamailio/kamailio.cfg
 cp /mnt/kamailio_voip/kamctlrc /usr/local/etc/kamailio/kamctlrc
 
+sleep 3
+
+mysql -uroot -p1234 -h 172.22.0.17 -e "ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '1234'; FLUSH PRIVILEGES;"
+
 echo "Starting Kamailio..."
 /usr/local/sbin/kamailio -DD -E
